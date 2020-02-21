eb3a9985c85e6044f3ab4ac35f423ff31e87bb3c
Vinicius <vinicius.tecbcc@gmail.com>
2020-02-21 18:19:09 -0300

    testando

--- src/main/java/ContatoServlet.java
+++ src/main/java/ContatoServlet.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/contato")
 public class ContatoServlet extends HttpServlet{
